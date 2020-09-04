ab4a9cb9375542aa73ed9d5c93aebb18d9e4300e
silveimar <silveimar.paez@oyster.io>
2020-09-03 21:16:59 -0500

    Adding basic maven project structure

---
+++ fizzbuzz/src/main/java/org/mxscc/fizzbuzz/App.java
@@ -0,0 +1,13 @@
+package org.mxscc.fizzbuzz;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
