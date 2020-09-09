51564b027c4bc8a8b28a45d2d3114d962f7bd808
Gerardo Zúñiga <gerardo.blupub@gmail.com>
2020-09-08 20:44:12 -0500

    Init leapyear-kata project.

---
+++ leapyear-kata/src/main/java/org/mxscc/leapyear/App.java
@@ -0,0 +1,13 @@
+package org.mxscc.leapyear;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
